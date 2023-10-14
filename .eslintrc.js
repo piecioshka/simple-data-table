d48f68d0ef61fed8b17fc15c436828a945cab30b
Piotr Kowalski <piecioshka@gmail.com>
2023-10-14 14:05:29 +0200

    Update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
         strict: ['off'],
         'object-curly-newline': ['off'],
         'no-magic-numbers': ['error', {
-            ignore: [0, 1, 3]
+            ignore: [-1, 0, 1, 3]
         }],
         'arrow-parens': ['off']
     },
